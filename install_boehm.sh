6f5774e0a5fbc3b040bd6d42367534ae67a6049c
Your Name <you@example.com>
2020-11-20 12:46:34 -0500

    Add uninstall option

--- install_boehm.sh
+++ install_boehm.sh
@@ -1,10 +1,22 @@
-echo Install the newest archive here: https://github.com/ivmai/bdwgc/releases
-echo "Once you're done put it in the lib/GC directory."
-read delay
-cd lib/GC
-tar xvfz *.gz
-cd *
-./configure --prefix=/usr/local/bin --disable-threads
-make
-make check
-make install
+install_boehm() {
+	echo Install the newest archive here: https://github.com/ivmai/bdwgc/releases
+	echo "Once you're done put it in the lib/GC directory."
+	read delay
+	cd lib/GC
+	tar xvfz *.gz
+	cd *
+	./configure --prefix=/usr/local/bin --disable-threads
+	make
+	make check
+	make install
+}
+
+remove_boehm() {
+	COMPILER_DIR=$PWD
+	rm -r lib/GC/*
+	cd /usr/local/bin/lib
+	rm libgc.dylib libgc.1.dylib libgc.la
+	cd $COMPILER_DIR
+}
+
+install_boehm
