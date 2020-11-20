52d3296d9c9ea45333db27302def7c98d12c6b83
Your Name <you@example.com>
2020-11-20 08:29:59 -0500

    _GC_malloc isn't freeing memory fast enough

--- tests/bugs.lisp
+++ tests/bugs.lisp
@@ -17,4 +17,6 @@
 ; For this to work, that lambda has to be able to capture its surrounding environment.
 ; 1. How would it do that?
 ; 2. What is making it behave not as expected right now? (or why is it squaring the first argument?)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Can't print results from rand32 (segfault)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Can't print results from rand32 (segfault)
+; _GC_malloc doesn't seem to free memory (memory just seems to be endlessly allocated)
