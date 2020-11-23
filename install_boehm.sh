99b4119414d80a2702d13aece640871a73a44a94
Your Name <you@example.com>
2020-11-23 14:26:19 -0500

    Simplify installation

--- install_boehm.sh
+++ install_boehm.sh
@@ -1,22 +1,12 @@
-install_boehm() {
-	echo Install the newest archive here: https://github.com/ivmai/bdwgc/releases
-	echo "Once you're done put it in the lib/GC directory."
-	read delay
-	cd lib/GC
-	tar xvfz *.gz
-	cd *
-	./configure --prefix=/usr/local/bin --disable-threads
-	make
-	make check
-	make install
-}
-
-remove_boehm() {
-	COMPILER_DIR=$PWD
-	rm -r lib/GC/*
-	cd /usr/local/bin/lib
-	rm libgc.dylib libgc.1.dylib libgc.la
-	cd $COMPILER_DIR
-}
-
-install_boehm
+version="gc-8.0.4"
+rm -r lib/GC/$version
+echo "Install the newest archive here: https://github.com/ivmai/bdwgc/releases"
+echo "Once you're done put it in the lib/GC directory."
+read delay
+cd lib/GC
+tar xvfz "${version}.tar.gz"
+cd $version
+./configure --prefix=/usr/local/bin --disable-threads
+make  # CFLAGS_EXTRA="-DGC_PRINT_VERBOSE_STATS"
+make check
+make install
