314432ffa763bf05d4bc6ecd296d77bb16956279
Caspian Ahlberg <caspianahlberg@gmail.com>
2020-12-07 09:02:32 -0500

    Make installation directory configurable

--- install_boehm.sh
+++ install_boehm.sh
@@ -1,4 +1,5 @@
 version="gc-8.0.4"
+install_dir="/usr/local/bin"
 rm -r lib/GC/$version
 echo "Install the newest archive here: https://github.com/ivmai/bdwgc/releases"
 echo "Once you're done put it in the lib/GC directory."
@@ -6,7 +7,7 @@ read delay
 cd lib/GC
 tar xvfz "${version}.tar.gz"
 cd $version
-./configure --prefix=/usr/local/bin --disable-threads
+./configure --prefix=$install_dir --disable-threads
 make  # CFLAGS_EXTRA="-DGC_PRINT_VERBOSE_STATS"
 make check
 make install
